cb0ded966d3a1dd005ab9e007b26f158bd5ae18b
oussama ouakassi <lmoutchoo@gmail.com>
2025-09-28 17:27:41 +0100

    refactor Header component to hide on scroll; update styles and add motion effects

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -5,6 +5,8 @@ import "./Header.css";
 import Image from "next/image";
 import { FaPhoneAlt } from "react-icons/fa";
 import Button from "../buttons/Button";
+import { useEffect, useState } from "react";
+import { motion } from "motion/react";
 
 const navLinks = [
   { title: "accueil", link: "accueil" },
@@ -12,18 +14,48 @@ const navLinks = [
 ];
 
 export default function Header() {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      console.log(currentScrollY);
+
+      // Hide header when scrolled down, show when at top
+      if (currentScrollY > 0) {
+        setIsVisible(false);
+      }
+      if (currentScrollY === 0) {
+        setIsVisible(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
   return (
-    <header className=" header">
+    <motion.header
+      layout
+      className=" header"
+      style={
+        !isVisible
+          ? { top: 10 }
+          : { top: "calc(var(--top-header-height) + 1rem )" }
+      }
+    >
       <nav className="container">
         <Logo />
         <NavList>
           {navLinks.map(({ title, link }) => (
-            <NavItem title={title} link={link} />
+            <NavItem key={title} title={title} link={link} />
           ))}
         </NavList>
         <Button icon={<FaPhoneAlt />} text={"+33 611 231 314"} />
       </nav>
-    </header>
+    </motion.header>
   );
 }
 
